8e191b85aa07ebc99fc17dda10da65365689c8e6
William Perreault <willyjames.music@gmail.com>
2017-09-14 17:40:41 -0400

    Update gulpfile.js

--- user/themes/jwillp/gulpfile.js
+++ user/themes/jwillp/gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp')
-var gutil        = require('gulp-util');
+var gutil = require('gulp-util');
 
 var sass = require('gulp-sass')
 var autoprefixer = require('gulp-autoprefixer')
